cf8f372ebcb386d7b6c26b5d71d817757b942c11
hoangngocquy <hoang.ngoc.quy@framgia.com>
2016-03-25 18:30:21 +0700

    completed my application

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,18 +1,28 @@
 class ImageController < ApplicationController
   def index
-  	if params[:search_keyword]
+  	@result_data_image_array = {}
 
+  	if params[:search_keyword]
+  		key_word = params[:search_keyword]
+  		flickAPIKey = '5149a64fa91469647a7511af9adf33a5'
+  		httpRequestUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='+flickAPIKey+'&text='+key_word+'&per_page=30&nojsoncallback=1&format=json'
+	  	require 'net/http'
+			result_data = Net::HTTP.get(URI.parse(httpRequestUrl))
+			result_data = JSON.parse(result_data)
+			if result_data["stat"] == "ok"
+				@result_data_image_array["notice"] = "success"
+				@result_data_image_array["data"] = result_data["photos"]["photo"]
+				@result_data_image_array["data"].each do |data_el|
+					data_el["url"] = "https://farm"+data_el["farm"].to_s+".staticflickr.com/"+data_el["server"]+"/"+data_el["id"]+"_"+data_el["secret"]+"_b.jpg"
+				end
+			else
+				@result_data_image_array["notice"] = "error"
+				@result_data_image_array["data"] = "No data return. Please try again in next time"
+			end
+			# byebug
   	else
 
   	end
-  	@image_result = []
-  	@image_result.push('http://www.keenthemes.com/preview/metronic/theme/assets/global/plugins/jcrop/demos/demo_files/image1.jpg')
-  	@image_result.push('http://www.biendao24h.vn/product_images/uploaded_images/anh-girl-xinh-9x-de-thuong-9-.jpg')
-  	@image_result.push('http://thuvienanhdep.net/wp-content/uploads/2015/11/nhung-hinh-anh-girl-xinh-dang-yeu-va-de-thuong-nhat-tren-facebook-4.jpg')
-  	@image_result.push('http://thuthuattienich.com/wp-content/uploads/2013/06/girl-cute-7.jpg')
-  	@image_result.push('http://images.tapchianhdep.net/15-12hinh-anh-girl-xinh-hoc-sinh-mac-dong-phuc-cuc-de-thuong3.jpg')
-  	@image_result.push('http://blogthugian.info/wp-content/uploads/2015/07/anh-girl-xinh-de-thuong-tren-facebook-6.jpg')
-  	@image_result.push('http://www.tech12h.com/sites/default/files/styles/inbody400/public/girl-xinh-1.jpg?itok=atdjp9Cw')
-  	@image_result.push('http://www.biendao24h.vn/product_images/m/438/anh_girl_xinh_96__43530_zoom.jpg')
-  end
+
+	end
 end
