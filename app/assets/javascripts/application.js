955cbb31f5bdbe1ee6d18fde81338217aefb8fa1
ngocquyhoang <ngocquyhoang.93@gmail.com>
2017-09-05 14:29:02 +0700

    Clean code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-// require turbolinks
-//
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
@@ -23,11 +10,13 @@ jQuery(document).ready(function($) {
 $(window).load(function() {
 	$(".pre-loader").fadeOut(500);
 });
+
 function setHeight() {
 	sHeight = $(document).height();
 	cHeight = (sHeight-45)/2-131;
 	$('.center-form').css('margin-top', cHeight);
 }
+
 // onload
 $(window).load(function(){
 	var mainImageWidth = $('.main-image').width();
